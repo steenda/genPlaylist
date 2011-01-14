baff084262449c7616e6223e3e5000ae188dc788
Anonymous <kornKoala@fy9.org>
2011-01-14 20:44:40 +0100

    m3u generation and verbose switch

--- genPlaylist.rb
+++ genPlaylist.rb
@@ -37,6 +37,12 @@ OptionParser.new do |opts|
    opts.on("-a", "--artist", "Get top tracks for specified artist") do
       options[:artist] = true
    end
+   opts.on("-m", "--m3u", "Generate m3u file") do
+      options[:m3u] = true
+   end
+   opts.on("-v", "--verbose", "Verbose output") do
+      options[:verbose] = true
+   end
    begin
       opts.parse!(ARGV)
    rescue OptionParser::ParseError => e
@@ -46,31 +52,34 @@ OptionParser.new do |opts|
    end
 end
 
-def makeTopTracks(current_artist,mpd)
+def genM3u(filename,playlist)
+   f = File.open("top_#{filename}.m3u","w")
+   playlist.each {|i| f.puts(i.file)}
+   f.close
+end
+
+def makeTopTracks(current_artist,mpd,options)
    artist = Rockstar::Artist.new(current_artist)
    track_arr = []
    new_playlist = []
-   added=0
-   puts "fetching top tracks..."
+   added = 0
+   track_arr = artist.top_tracks
    own_songs = mpd.search('artist', artist.name)
-   artist.top_tracks.each { |t| track_arr << "#{t.name}" }
-   puts "checking for existing tracks..."
 
    for i in track_arr do
-      index = own_songs.index {|x| Iconv.iconv('utf-8','iso-8859-15',x.title.downcase) == Iconv.iconv('utf-8','iso-8859-15',i.downcase) }
+      index = own_songs.index {|j| Iconv.conv('utf-8','iso-8859-15',j.title.downcase) == Iconv.conv('utf-8','iso-8859-15',i.name.downcase) }
       if index != nil
-         new_playlist << own_songs[index].file
+         new_playlist.push(own_songs[index].file)
          added += 1
+         if options[:verbose]
+            puts "+ #{own_songs[index]}"
+         end
       end
    end
    mpd.clear
-   for i in new_playlist do
-      mpd.add(i)
-   end
-   if !mpd.playlist().empty?
-      mpd.play(0)
-   end
-   puts "added #{added.to_s} songs!"
+   new_playlist.each {|i| mpd.add(i)}
+
+   return added
 end
 
 # main
@@ -83,6 +92,14 @@ else
    exit 1
 end
 
-makeTopTracks(current_artist,mpd)
+added = makeTopTracks(current_artist,mpd,options)
+if added > 0
+   mpd.play(0)
+   if options[:m3u]
+      genM3u(mpd.current_song().artist,mpd.playlist())
+   end
+else
+   STDERR.puts "No tracks added!"
+end
 
 exit 0
