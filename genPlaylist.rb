c73eb1e9b1368e0ce93aaf4f58c4e1ec813908c6
Anonymous <kornKoala@fy9.org>
2011-02-17 20:02:41 +0100

    new song class

--- genPlaylist.rb
+++ genPlaylist.rb
@@ -27,148 +27,165 @@ require 'yaml'
 API_KEY = "b25b959554ed76058ac220b7b2e0a026"
 BASE_URL = "http://ws.audioscrobbler.com/2.0/"
 
+# classes
+class Song
+  attr_accessor :title
+  attr_accessor :artist
+
+  def initialize(*args)
+    if args.size < 1  || args.size > 2
+      puts 'Song.initialize: wrong parameter count!'
+    else
+      if args.size == 1
+    @title = Iconv.conv('utf-8','iso-8859-15',REXML::XPath.first(args[0], "name").text)
+    @artist = Iconv.conv('utf-8','iso-8859-15',REXML::XPath.first(args[0], "artist/name").text)
+      else
+    @title = args[0]
+    @artist = args[1]
+      end
+    end
+  end
+
+end
+
 # functions
 def genM3u(filename,playlist)
-   f = File.open("top_#{filename}.m3u","w")
-   playlist.each {|i| f.puts(i.file)}
-   f.close
+  f = File.open("top_#{filename}.m3u","w")
+  playlist.each {|i| f.puts(i.file)}
+  f.close
 end
 
 def queryLastFM(query)
-   track_arr = []
-   source = URI.parse(BASE_URL)
-   source.query = query  + "&api_key=#{API_KEY}"
-   parsed = REXML::Document.new(Net::HTTP.get(source))
-   case query
-   when
-      if query =~ /^.*artist\.gettoptracks.*$/
-         parsed.elements.each("lfm/toptracks/track") { |element| track_arr.push(Hash['title', element[1].text, 'artist', element[15][1].text]) }
-      end
-   when
-      if query =~ /^.*track\.getsimilar.*$/
-         parsed.elements.each("lfm/similartracks/track") { |element| track_arr.push(Hash['title', element[1].text, 'artist', element[15][1].text]) }
-      end
-   when
-      if query =~ /^.*tag\.gettoptracks.*$/
-         parsed.elements.each("lfm/toptracks/track") { |element| track_arr.push(Hash['title', element[1].text, 'artist', element[11][1].text]) }
-      end
-   end
+  track_arr = []
+  source = URI.parse(BASE_URL)
+  source.query = query  + "&api_key=#{API_KEY}"
+  parsed = REXML::Document.new(Net::HTTP.get(source))
 
-   return track_arr
+  case query
+  when /artist\.gettoptracks/
+      parsed.elements.each("lfm/toptracks/track") { |element| track_arr.push(Song.new(element)) }
+  when /track\.getsimilar/
+      parsed.elements.each("lfm/similartracks/track") { |element| track_arr.push(Song.new(element)) }
+  when /tag\.gettoptracks/
+      parsed.elements.each("lfm/toptracks/track") { |element| track_arr.push(Song.new(element)) }
+  end
+
+  return track_arr
 end
 
 def addExistingTracks(track_arr, mpd)
-   artist_arr = []
-   new_playlist = []
-   own_songs = []
-   tmp = []
-   added = 0
+  artist_arr = []
+  new_playlist = []
+  own_songs = []
+  tmp = []
+  added = 0
 
-   track_arr.each{|x| artist_arr.push(Hash['artist', x['artist']])}
-   for i in artist_arr.uniq do
-      tmp = mpd.search('artist', i['artist'])
-      if tmp.empty?
-         track_arr.delete_if {|x| x['artist'] == i['artist'] }
-      else
-         own_songs += tmp
-      end
-   end
+  track_arr.each{|x| artist_arr.push(x.artist)}
+  for i in artist_arr.uniq do
+    tmp = mpd.search('artist', i)
+    if tmp.empty?
+      track_arr.delete_if {|x| x.artist == i }
+    else
+      own_songs += tmp
+    end
+  end
 
-   for i in track_arr do
-      index = own_songs.index {|j| Iconv.conv('utf-8','iso-8859-15',j.title.downcase) == Iconv.conv('utf-8','iso-8859-15',i['title'].downcase) }
-      if index != nil
-         new_playlist.push(own_songs[index].file)
-         added += 1
-      end
-   end
-   mpd.clear
-   new_playlist.each {|j| mpd.add(j)}
+  for found in track_arr do
+    index = own_songs.index {|own| Iconv.conv('utf-8','iso-8859-15',own.title.downcase) == found.title.downcase }
+    if index != nil
+      new_playlist.push(own_songs[index].file)
+      added += 1
+    end
+  end
+  mpd.clear
+  new_playlist.each {|j| mpd.add(j)}
 
-   return added
+  return added
 end
 
 def makeTopTracks(current_artist,mpd)
-   query = "method=artist.gettoptracks" + "&artist=#{URI.encode(current_artist)}"
-   track_arr = queryLastFM(query)
+  query = "method=artist.gettoptracks" + "&artist=#{URI.encode(current_artist)}"
+  track_arr = queryLastFM(query)
 
-   return addExistingTracks(track_arr, mpd)
+  return addExistingTracks(track_arr, mpd)
 end
 
 def makeSimilarTracks(mpd)
-   query = "method=track.getsimilar" + "&artist=#{URI.encode(mpd.current_song().artist)}" + "&track=#{URI.encode(mpd.current_song().title)}"
-   track_arr = queryLastFM(query)
+  query = "method=track.getsimilar" + "&artist=#{URI.encode(mpd.current_song().artist)}" + "&track=#{URI.encode(mpd.current_song().title)}"
+  track_arr = queryLastFM(query)
 
-   return addExistingTracks(track_arr, mpd)
+  return addExistingTracks(track_arr, mpd)
 end
 
 def makeTopTags(tag,mpd)
-   query = "method=tag.gettoptracks" + "&tag=#{URI.encode(tag)}"
-   track_arr = queryLastFM(query)
+  query = "method=tag.gettoptracks" + "&tag=#{URI.encode(tag)}"
+  track_arr = queryLastFM(query)
 
-   return addExistingTracks(track_arr, mpd)
+  return addExistingTracks(track_arr, mpd)
 end
 
 # main
 added = 0
 options = {}
 if File.exist?(File.dirname(__FILE__)+'/genPlaylist.conf')
-   conf = YAML.load_file(File.dirname(__FILE__)+'/genPlaylist.conf')
-   mpd = MPD.new(conf['mpd_host'], conf['mpd_port'])
+  conf = YAML.load_file(File.dirname(__FILE__)+'/genPlaylist.conf')
+  mpd = MPD.new(conf['mpd_host'], conf['mpd_port'])
 else
-   mpd = MPD.new("localhost", 6600)
+  mpd = MPD.new("localhost", 6600)
 end
 mpd.connect
 
 OptionParser.new do |opts|
-   opts.banner = "Usage: #{File.basename($0)}"
-   opts.on("-h", "--help", "Displays this help info") do
-      puts opts
-      exit 0
-   end
-   opts.on("-a", "--artist", "Get top tracks for specified artist") do
-      options[:artist] = true
-      if ARGV[0] != nil
-         added = makeTopTracks(ARGV[0],mpd)
-      elsif !mpd.playlist().empty?
-         added = makeTopTracks(mpd.current_song().artist,mpd)
-      else
-         STDERR.puts "No track specified!"
-         exit 1
-      end
-   end
-   opts.on("-s", "--similar", "Get similar tracks for currently playing song") do
-      options[:similar] = true
-      if !mpd.playlist().empty?
-         added = makeSimilarTracks(mpd)
-      else
-         STDERR.puts "No track playing!"
-         exit 1
-      end
-   end
-   opts.on("-t", "--tag", "Get top tracks for specified tag") do
-      options[:tag] = true
-      param = ARGV[0]
-      added = makeTopTags(param,mpd)
-   end
-   opts.on("-m", "--m3u", "Generate m3u file") do
-      options[:m3u] = true
-   end
-   begin
-      opts.parse!(ARGV)
-   rescue OptionParser::ParseError => e
-      warn e.message
-      puts opts
+  opts.banner = "Usage: #{File.basename($0)}"
+  opts.on("-h", "--help", "Displays this help info") do
+    puts opts
+    exit 0
+  end
+  opts.on("-a", "--artist", "Get top tracks for specified artist") do
+    options[:artist] = true
+    if ARGV[0] != nil
+      added = makeTopTracks(ARGV[0],mpd)
+    elsif !mpd.playlist().empty?
+      added = makeTopTracks(mpd.current_song().artist,mpd)
+    else
+      STDERR.puts "No track specified!"
       exit 1
-   end
+    end
+  end
+  opts.on("-s", "--similar", "Get similar tracks for currently playing song") do
+    options[:similar] = true
+    if !mpd.playlist().empty?
+      added = makeSimilarTracks(mpd)
+    else
+      STDERR.puts "No track playing!"
+      exit 1
+    end
+  end
+  opts.on("-t", "--tag", "Get top tracks for specified tag") do
+    options[:tag] = true
+    param = ARGV[0]
+    added = makeTopTags(param,mpd)
+  end
+  opts.on("-m", "--m3u", "Generate m3u file") do
+    options[:m3u] = true
+  end
+  begin
+    opts.parse!(ARGV)
+  rescue OptionParser::ParseError => e
+    warn e.message
+    puts opts
+    exit 1
+  end
 end
 
 if added > 0
-   mpd.play(0)
-   if options[:m3u]
-      genM3u(mpd.current_song().artist,mpd.playlist())
-   end
+  mpd.play(0)
+  if options[:m3u]
+    genM3u(mpd.current_song().artist,mpd.playlist())
+  end
 else
-   STDERR.puts "Could not generate playlist!"
+  STDERR.puts "Could not generate playlist!"
 end
 
 exit 0
+
